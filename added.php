da5c123648a9604d73e8e7a6ce210e322196a251
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-06-04 00:47:51 +0300

    Added feature to add a book to the shopping cart

--- added.php
+++ added.php
@@ -4,8 +4,19 @@ include ('includes/session.php');
 if (isset ($_GET ['id'])) $id = $_GET ['id'];
 
 require ('db_connection.php');
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/added.css" /> 
+    <title>Welcome to Coffee and Books </title>
+  </head>
+<body>
 
-$query = "SELECT from books WHERE book_id = $id";
+<?php
+$query = "SELECT * from books WHERE book_id = $id";
 $result = mysqli_query ($link, $query);
 
 if (mysqli_num_rows ($result) == 1){
@@ -15,21 +26,41 @@ if (mysqli_num_rows ($result) == 1){
 if (isset ($_SESSION ['cart'][$id]))
 {
     $_SESSION ['cart'][$id]['quantity'] ++;
-    echo '
-    <div class ="container">
-            <p>Another '.$row["item_name"].' has been added to your cart</p>
-			<a href="home.php">Continue Shopping</a> | <a href="cart.php">View Your Cart</a>
-    </div>;
-    '
+?>
+
+<div class="full-screen-container">
+    <div class = "added-container">
+
+    <h1 class="added-title" > Congratulations! </h1>
+
+            <div class="input-group">
+                <p>One more "<?php echo $row ['book_name']; ?>"  by <?php echo $row ['book_author']; ?>  was added to your cart</p>
+                <a href="home.php">Continue Shopping</a> | <a href="cart.php">View Your Cart</a>
+               
+            </div>
+    </div>  
+</div>';
+
+<?php    
 }
 else 
 {
    $_SESSION['cart'][$id] = array ('quantity'=> 1, 'price'=> $row['item_price']);
-   echo '<div class = "container">
-        <p> A '$row ['book_name'].' by '$row ['book_author'].' was added to you cart </p>
-        <a href="home.php">Continue Shopping</a> | <a href="cart.php">View Your Cart</a>
-   </div>;
-   '
+
+ ?>
+        <div class="full-screen-container">
+            <div class = "added-container">
+
+            <h1 class="added-title" > Congratulations!  </h1>
+
+                    <div class="input-group">
+                      <p> A "<?php echo $row ['book_name']; ?>"  by <?php echo $row ['book_author']; ?>  was added to your cart</p>
+                        <a href="home.php">Continue Shopping</a> | <a href="cart.php">View Your Cart</a>
+                    
+                    </div>
+            </div>  
+        </div>';
+<?php        
 }
 
 ?>
--- cart.php
+++ cart.php
@@ -1,4 +1,73 @@
 <?php
 include ('includes/session.php');
 include ('includes/nav.php');
+//include ('added.php');
 ?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    foreach  ($_POST ['quantity'] as $book_id=> $book_qty)
+    {
+        $id = (int)$book_id;
+        $qty = (int) $book_qty;
+    }
+
+    if ($qty == 0){
+        unset ($_SESSION ['cart'][$id]);}
+    elseif ($qty > 0){
+        $_SESSION ['cart'][$id]['quantity'] = $qty;
+    }   
+}
+
+$total = 0;
+
+if (!empty ($_SESSION ['cart']))
+{
+    require ('db_connection.php');
+
+    $query = "SELECT * from books WHERE book_id IN (";
+    foreach ($_SESSION['cart'] as $id => $value) { $query .= $id . ','; }
+
+    $query = substr ($query, 0, -1) . ") ORDER BY book_id ASC";
+}
+
+
+$result = mysqli_query ($link, $query);
+
+//display container
+echo '<div class = "container" 
+    <h2> Shopping cart </h2>
+    <form action = "cart.php" method = "post">;
+            </div>';
+
+        while ($row = mysqli_fetch_assoc ($result))
+        {
+            //calculate sub-total
+            $subtotal = $_SESSION ['cart'] [$row ['book_id']]['quantity'] * $_SESSION ['cart'] [$row ['book_id']]['price'];
+            $total += $subtotal;
+
+            echo 
+            " <img src = \"./img/{$row['book_img']}\">
+                
+                <div class = \"name\"> {$row ['book_name']} </div> 
+                <div class = \"card_price\"> {$row ['item_price']} </div>
+                <input type=\"text\" 
+                size=\"3\" 
+                name=\"[{$row['book_id']}]\" 
+                value=\"{$_SESSION['cart'][$row['book_id']]['quantity']}\">
+                
+            ";
+        }
+        mysqli_close ($link);
+
+            echo '
+                
+                <div>
+                <p>Total = &pound '.number_format($total,2).'</p>
+                </div>
+
+                <p><input type="submit" name="submit" class="update-btn" value="Update My Cart"></p>
+                <br>
+                <a href="checkout.php?total='.$total.'" class="checkout-btn">Checkout Now</a><br>
+    </form>' ;
