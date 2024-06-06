0e8eaefa32c0ab8c86553854e608061e7c944271
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-06-06 20:57:37 +0300

    refactored

--- cart.php
+++ cart.php
@@ -93,7 +93,7 @@ if (!empty ($_SESSION ['cart']))
                         
                         ?>
                             <div class = "total-group">
-                                <p class = "total"> Total = &pound $total</p><br>
+                                <p class = "total"> Total = &pound <?php echo $total ;?></p><br>
                                 <p><input type="submit" name="submit" class="update-btn" value="Update My Cart"></p>
                                 <br>
                                 <a href="checkout.php?total=<?php echo $total;?>" class="checkout-btn">Checkout Now</a><br>
