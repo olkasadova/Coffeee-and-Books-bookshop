2140db6b464a6d3ef26d1f38b09bfd00562584e6
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-06-10 22:55:57 +0300

    Added assertion to verify passed data in Cypress

--- cypress/e2e/Add-to-cart-tests.cy.js
+++ cypress/e2e/Add-to-cart-tests.cy.js
@@ -1,28 +1,36 @@
 // test that one item can be successfully added to user's shopping cart
 
-describe('Home page redirect for unlogged user', () => {
-    it('visit the Home page', () => {
-      cy.visit('http://localhost:5500/home.php')
-     
-      //login with valid email and passowrd
-      cy. login ('olha@gmail.com', '12345')
+describe('Home page redirect for unlogged user', () => 
+  {
+    it('visit the Home page', () => 
+      {
+        
+        cy.visit('http://localhost:5500/home.php')
+      
+        //login with valid email and passowrd
+        cy. login ('olha@gmail.com', '12345')
+        
+        //get author value of the 0-indexed item (author and price)
+        cy.get('[data-cy = "card_author"]').eq(0).then(($div) => {
+          const author = $div.text();
+          cy.wrap (author).as('author');
+        });
+        
+        //click the link with 0 index
+        cy.get('[data-cy = "add-cart"]').eq(0).click()
+        
+      //check that user is redirected to Add to Cart page and has 
+      
+        cy.url().should('include', '/added')
+        cy.get('.input-group').get ('p').contains ("added to your cart")
 
-      //get values of the 0-indexed item (author and price)
-      cy.get('[data-cy = "card_author"]').eq(0).then(($div) => {
-        const author = $div.text();
-        cy.log (author);
-        cy.wrap(author).as('author')})
-
-      //click the link with 0 index
-      cy.get('[data-cy = "add-cart"]').eq(0).click()
-
-     //check that user is redirected to Add to Cart page and has 
-      cy.url().should('include', '/added')
-      cy.get('.input-group').get ('p').contains ("added to your cart")
-     // cy.get('.input-group').get ('p').should('eq','@author')
-
-      cy.clickLink("View Your Cart")
-
-    //  cy.log("Redirected from Home page to Login")
+        // verify that author value got on the Home page is present in the Added message
+        cy.get('@author').then(auth=> {
+          cy.get('.input-group').get ('p').should('contain', auth)
+          cy.log (auth);
+        })
+       
+        cy.clickLink("View Your Cart")
+        cy.url().should('include', '/cart')
     })
   })
